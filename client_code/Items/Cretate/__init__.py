78b78535e7bbaa2415b29a05634a1bb238eab0d6
ict@socialit.it <ict@socialit.it>
2024-03-13 10:10:27 +0000

    Created ServerModule1, Items and Items.Cretate; Edited settings; Deleted Form1

--- client_code/Items/Cretate/__init__.py
+++ client_code/Items/Cretate/__init__.py
@@ -1,12 +1,19 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import CretateTemplate
 from anvil import *
+import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Form1(Form1Template):
+class Cretate(CretateTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def btnCreate_click(self, **event_args):
+    
+    
+    
+    pass
