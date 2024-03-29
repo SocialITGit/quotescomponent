f1208e34e4dbec85e1b91e1d12507418b9cb203c
ict@socialit.it <ict@socialit.it>
2024-03-29 10:37:15 +0000

    Created Configurations.ConfigurationsCard; Edited Configurations.ConfigurationsHome; Deleted Configurations.ConfigurationCard

--- client_code/Configurations/ConfigurationsCard/__init__.py
+++ client_code/Configurations/ConfigurationsCard/__init__.py
@@ -1,11 +1,11 @@
-from ._anvil_designer import ConfigurationCardTemplate
+from ._anvil_designer import ConfigurationsCardTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class ConfigurationCard(ConfigurationCardTemplate):
+class ConfigurationsCard(ConfigurationsCardTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
